ec956ce58a6364a176e7daad2ab427200734db99
AhmedShab <a.shaban@qetaa.com>
2019-01-28 12:40:27 +0300

    updated the vehicle api

--- routes/api/index.js
+++ routes/api/index.js
@@ -5,7 +5,7 @@ const V2 = '/v2';
 router.use(`${V1}/partForm`, require('./v1/partForm/partForm'));
 router.use(`${V2}/customer`, require('./v2/customer'));
 router.use(`${V2}/location`, require('./v2/location'));
-router.use(`${V1}/vehicle`, require('./v1/vehicle'));
+router.use(`${V2}/vehicle`, require('./v2/vehicle'));
 router.use(`${V2}/cart`, require('./v2/cart'));
 router.use(`${V2}/product`, require('./v2/product'));
 
