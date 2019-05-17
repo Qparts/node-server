65bce54307b75c6593291d437a8a6776ff38b943
fareed <fareed@qetaa.com>
2019-05-17 17:33:24 +0300

    changed session duration to 7 days

--- middlewares/sessionMiddleware.js
+++ middlewares/sessionMiddleware.js
@@ -1,12 +1,12 @@
 const session = require('express-session');
 const crypto = require('crypto');
 
-const eightHours = 28800000;
+const age = 604800000;
 
 module.exports = session({
 	secret: crypto.randomBytes(12).toString('hex'),
 	cookie: {
-		maxAge: eightHours
+		maxAge: age
 	},
 	resave: false,
 	saveUninitialized: true,
